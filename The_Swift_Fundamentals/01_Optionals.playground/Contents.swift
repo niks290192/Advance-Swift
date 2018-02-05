f63b346dbb666837deeb9828f58c4a7e6a373397
NiKhil AroRa <nikhil290192@gmail.com>
2018-02-05 22:38:38 +0530

    Started Optionals
    Started Optionals

---
+++ The_Swift_Fundamentals/01_Optionals.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+//: Problem
+// Why Do Swift Engineers implement Optionals
+
+//: Swift Rules
+// 1. Every variable type must be defined(Implicit/Explicit)
+// 2. The type is inferred based on the value
+
+// String
+let name: String = "Nikhil" //Explicit
+let newName = "Nikhil the Developer" //Implicit

b2b57f7fa447487663e93c0a2d2357b2c48a134b
NiKhil AroRa <nikhil290192@gmail.com>
2018-08-30 00:09:30 +0530

    completed Error Handling

--- The Swift Fundamentals/OptionalChaining.playground/Contents.swift
+++ The Swift Fundamentals/OptionalChaining.playground/Contents.swift
@@ -1,5 +1,45 @@
-//: Playground - noun: a place where people can play
+/*:
+ ## Learn Swift
+ ### Chapter 1: The Swift Fundamentals
+ ### Optional Chainings
+ 
+ **Problem:** Why do I see "?" and "!" when accessing methods and properties?
+ 
+ ---
+*/
 
+//: > Example from UIKit
 import UIKit
+let labelColor = UILabel().highlightedTextColor?.cgColor
 
-var str = "Hello, playground"
+//: Design Class
+class Human {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func sayHello() {
+        print("Hello, I'm \(name)")
+    }
+}
+
+//: Design Another Class
+class Apartment {
+    var human: Human?
+}
+
+//: Initialization
+var delhiApartment = Apartment()
+delhiApartment.human = Human(name: "Nikhil")
+
+delhiApartment.human?.sayHello()
+if let residentName = delhiApartment.human?.name {
+    print("\(residentName)")
+} else {
+    
+}
+
+var NYCApartment = Apartment()
+// let NYCApartment = NYCApartment.human!.name

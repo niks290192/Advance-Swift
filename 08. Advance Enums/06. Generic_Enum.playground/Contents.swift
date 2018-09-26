2a75ab8891a1f6d561c09a4367954d88b8915a52
NiKhil AroRa <nikhil290192@gmail.com>
2018-09-26 20:22:01 +0530

    genericEnumCompleted

---
+++ 08. Advance Enums/06. Generic_Enum.playground/Contents.swift
@@ -0,0 +1,44 @@
+/*:
+ ## Learn Swift
+ ### Advance Enum
+ ### Generic Enum
+ 
+ **Problem:** A little more complex associate Value
+ ---
+ */
+
+//: Introducing Generic Enum
+enum Information<T, U> {
+    case name(T)
+    case website(T)
+    case age(U)
+}
+
+// Information.name("Bob")
+// T is "String", but U?
+
+Information<String, Int>.age(20)
+
+//: Optionals with Enum
+let explicitOptionals = Optional("Niks")
+let quickOptionals: String? = "Niks"
+
+print(explicitOptionals)
+print(quickOptionals)
+
+//: Custom Optionals
+
+enum MyOptional<T> {
+    case none // nil
+    case some(T)
+    
+    init(_ anyValue: T) {
+        self = MyOptional.some(anyValue)
+    }
+}
+
+MyOptional("Nikhil")
+
+// if let value = myOptional {
+//      print(value)
+// }
